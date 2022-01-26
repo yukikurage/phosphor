2d6f8da0e6f7d7fd05a76ac242c8a3cf4c78857a
yukikurage <55534323+yukikurage@users.noreply.github.com>
2022-01-26 15:35:49 +0900

    apply formatter

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,9 @@
 module Main where
 
+f x = case x of
+  Nothing -> Nothing
+  Just y  -> Just y
+
 main :: IO ()
-main = pure ()
+main = do
+  pure ()
--- src/Phosphor/Data/AST.hs
+++ src/Phosphor/Data/AST.hs
@@ -1,6 +1,6 @@
 module Phosphor.Data.AST where
 
-import           Data.Text                      ( Text )
+import           Data.Text (Text)
 
 -- コードにおける位置 エラーハンドリングに使う getOffset で取得
 type CodePos = Int
@@ -8,45 +8,39 @@ type CodePos = Int
 type Variable = String
 
 data AST = AST Text -- Foreign
-                    [Statement]
+               [Statement]
 
-data Statement =
-                StatementDefinition CodePos Definition
-        | StatementData CodePos Variable [Constructor]
+data Statement = StatementDefinition CodePos Definition
+               | StatementData CodePos Variable [Constructor]
 
 data Constructor = Constructor CodePos Variable Type
 
 data Definition = Definition CodePos Pattern Expression
 
-data Type =
-          TypeVariable CodePos Variable
-        | TypeFunction CodePos Type Type
+data Type = TypeVariable CodePos Variable
+          | TypeFunction CodePos Type Type
 
-data Pattern =
-    PatternVariable CodePos Variable Type
-  | PatternWildCard CodePos Type
-  | PatternConstructor CodePos Variable
-        | PatternApply CodePos Pattern
+data Pattern = PatternVariable CodePos Variable Type
+             | PatternWildCard CodePos Type
+             | PatternConstructor CodePos Variable
+             | PatternApply CodePos Pattern
 
-data Expression =
-    ExpressionApply CodePos Expression Expression
-  | ExpressionVariable CodePos Variable Type
-  | ExpressionMatch CodePos [([Pattern], Expression)]
-  | ExpressionForeign CodePos Text Type
-  | ExpressionLiteral CodePos Literal
-  | ExpressionDo CodePos Do
-  | ExpressionLet CodePos Let
+data Expression = ExpressionApply CodePos Expression Expression
+                | ExpressionVariable CodePos Variable Type
+                | ExpressionMatch CodePos [([Pattern], Expression)]
+                | ExpressionForeign CodePos Text Type
+                | ExpressionLiteral CodePos Literal
+                | ExpressionDo CodePos Do
+                | ExpressionLet CodePos Let
 
-data Do =
-    DoReturn CodePos Expression
+data Do = DoReturn CodePos Expression
         | DoBind CodePos Pattern Expression Do
         | DoLet CodePos Pattern Expression Do
 
 data Let = Let CodePos [Definition] Expression
 
-data Literal =
-    LiteralString Text
-  | LiteralBool Bool
-  | LiteralChar Char
-  | LiteralInt Int
-  | LiteralFloat Float
+data Literal = LiteralString Text
+             | LiteralBool Bool
+             | LiteralChar Char
+             | LiteralInt Int
+             | LiteralFloat Float
